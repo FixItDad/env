85ddc35610fb3ea3399107ed14daf65f459ba02e
Paul Sparks <16521193+FixItDad@users.noreply.github.com>
2018-03-20 17:00:15 -0400

    Update python.py

--- templates/python.py
+++ templates/python.py
@@ -11,3 +11,19 @@ log.addHandler(handler)
 log.info('Some message')
 log.debug('Some debug')
 
+
+# Singleton
+class Singleton(type):
+    _instances = {}
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
+
+#Python2
+class MyClass(BaseClass):
+    __metaclass__ = Singleton
+
+#Python3
+class MyClass(BaseClass, metaclass=Singleton):
+    pass
